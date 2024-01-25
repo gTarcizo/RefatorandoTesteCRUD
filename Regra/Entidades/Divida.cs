2a468937820d4de7443af11840ded0c5d85cfa19
Tarcizo <90656673+gTarcizo@users.noreply.github.com>
2024-01-24 22:48:24 -0300

    Tabelad de divida adicionadas

---
+++ Regra/Entidades/Divida.cs
@@ -0,0 +1,15 @@
+﻿using Regra.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Regra.Entidades
+{
+   public class Divida
+   {
+
+      public int IdPessoa { get; set; }
+      public int IdDivida { get; set; }
+      public string DescricaoDivida { get; set; }
+      public float ValorDivida { get; set; }
+      public float? Juros { get; set; }
+   }
+}
---
+++ Regra/Models/DividaModel.cs
@@ -0,0 +1,39 @@
+﻿using Regra.Entidades;
+using System.ComponentModel.DataAnnotations;
+
+namespace Regra.Models
+{
+   public class DividaModel
+   {
+      public DividaModel()
+      {
+
+      }
+
+      public int IdPessoa { get; set; }
+      public int IdDivida { get; set; }
+      public string DescricaoDivida { get; set; }
+      public float ValorDivida { get; set; }
+      public float? Juros { get; set; }
+
+      public void EntidadeParaModel(Divida endereco)
+      {
+         IdPessoa = endereco.IdPessoa;
+         IdDivida = endereco.IdDivida;
+         DescricaoDivida = endereco.DescricaoDivida;
+         ValorDivida = endereco.ValorDivida;
+         Juros = endereco.Juros;
+      }
+      public Divida ModeloParaEntidade(Divida endereco)
+      {
+         return new Divida()
+         {
+            IdPessoa = endereco.IdPessoa,
+            IdDivida = endereco.IdDivida,
+            DescricaoDivida = endereco.DescricaoDivida,
+            ValorDivida = endereco.ValorDivida,
+            Juros = endereco.Juros,
+         };
+      }
+   }
+}
